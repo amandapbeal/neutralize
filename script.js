3ec679010ca58074f240d153d4754fb5229d3d8f
lemessdavi <ndlemes7@outlook.com>
2021-06-08 14:21:43 -0300

    criada a function para corrigir os levels

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-// oi!
+// função de correção de level
+function corrigeLevel(ladoEsquerdo, ladoDireito) {
+    if (ladoEsquerdo == ladoDireito) {
+        return true
+    } else {
+        return false
+    }
+
+}
