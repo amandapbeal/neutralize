e2a1945718f71280e36cf5beb9eae1a216d1dda2
lemessdavi <ndlemes7@outlook.com>
2021-06-08 14:41:39 -0300

    linkado o script com o html, sugestões para o corrigeLevel()

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 // função de correção de level
 function corrigeLevel(ladoEsquerdo, ladoDireito) {
+
+    //ladoEsquerdo.containsAll( ladoDireito )
     if (ladoEsquerdo == ladoDireito) {
         return true
     } else {
         return false
     }
-
 }
+
+//função return elementos separados
+// collection.containsAll( otherCollection )
